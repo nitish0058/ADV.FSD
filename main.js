851ef2aeda05dc2d468f5229e88d360dc6dba3f5
Nitish kumar yadav <n55719271@gmail.com>
2026-09-07 21:26:01 +0530

    function using module

---
+++ main.js
@@ -0,0 +1,11 @@
+function calculate_area(radius)
+{
+    return 3.14*radius*radius;
+}
+function calculate_perimeter(radius)
+{
+    return 2*3.14*radius;
+}
+
+module.exports=calculate_area
+module.exports=calculate_perimeter
